1d74ffe7577379b418dab95487531d608469966c
minimo-io <nicolas@minimo.io>
2024-04-08 16:43:13 -0300

    Basic dynamic structure!

--- _data/languages.js
+++ _data/languages.js
@@ -1,12 +1,21 @@
 module.exports = {
     es: {
         dir: "", // stands for the direction of the language set in the head, defaults to LTR (left to right)
+        menu: {
+            class: "dropdown-item font-weight-bold flag-icon-background flag-icon-es",
+        },
         availableText: "Esta página también está disponible en:",
     },
     "pt-br": {
+        menu: {
+            class: "dropdown-item flag-icon-background flag-icon-br",
+        },
         availableText: "Também disponivel em: ",
     },
     en: {
+        menu: {
+            class: "dropdown-item flag-icon-background flag-icon-gb",
+        },
         availableText: "This page is also available in:",
     },
 };
--- eleventy.config.js
+++ eleventy.config.js
@@ -16,9 +16,9 @@ module.exports = function (eleventyConfig) {
     // Copy the contents of the `public` folder to the output folder
     // For example, `./public/css/` ends up in `_site/css/`
     eleventyConfig.addPassthroughCopy({
-        "./public/": "/",
+        "./public/": "/assets/",
         "./node_modules/prismjs/themes/prism-okaidia.css":
-            "/css/prism-okaidia.css",
+            "/assets/css/prism-okaidia.css",
     });
 
     // Run Eleventy when these files change:
