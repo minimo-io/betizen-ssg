d3c69378b146ce7d9679dd142794ad3590f5a939
minimo-io <nicolas@minimo.io>
2024-04-08 22:59:44 -0300

    Language switcher fix

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ Reliable listings of online casinos, games, bonuses and affiliate programs, with
 
 ## To Do
 
--   Configure postcss and new pipeline, https://mxb.dev/blog/eleventy-asset-pipeline/
--   Create a filter to filter tags by language name insted of adding if statements (gameslist)
+-   Translate common texts
 -   Create staging repo
 -   Work on the tags/categories page
 -   Uncomment {# <meta name="robots" content="index,follow" /> #} and remove vercel.json noindex header.
+
+### v2
+
+-   Create a filter to filter tags by language name insted of adding if statements (gameslist)
--- _data/languages.js
+++ _data/languages.js
@@ -3,18 +3,21 @@ module.exports = {
         dir: "", // stands for the direction of the language set in the head, defaults to LTR (left to right)
         menu: {
             class: "dropdown-item font-weight-bold flag-icon-background flag-icon-es",
+            name: "Español",
         },
         availableText: "Esta página también está disponible en:",
     },
     "pt-br": {
         menu: {
             class: "dropdown-item flag-icon-background flag-icon-br",
+            name: "Português",
         },
         availableText: "Também disponivel em: ",
     },
     en: {
         menu: {
             class: "dropdown-item flag-icon-background flag-icon-gb",
+            name: "English",
         },
         availableText: "This page is also available in:",
     },
